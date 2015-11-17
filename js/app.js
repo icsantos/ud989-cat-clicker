f3f79f06fef4652a0b33adf779dfbb8560b3fa6c
icsantos <isantos1206@gmail.com>
2015-11-17 02:52:08 -0600

    feat: Add CuteAnimal superclass and Kitten class
    Add CuteAnimal superclass
    - has properties for the image url, id, name, click counter
    - has method to render the image and name on the screen
    - has a click event handler
    
    Add Kitten class
    - extends CuteAnimal

--- js/app.js
+++ js/app.js
@@ -1,7 +1,89 @@
+/**
+ * The number of kittens to display
+ * @constant
+ * @type {number}
+ * @default
+ */
 var TOTAL_KITTENS = 2;
 
-var clicks = 0;
+/**
+ * The container for displaying the cute animal
+ * @type {jQuery}
+ */
+var $main = $('main');
 
+// --------------------------------------------------
+// CuteAnimal superclass
+// --------------------------------------------------
+/**
+ * @constructor
+ * @classdesc A superclass of the cute animals we will use in the app.
+ * @param {kittens} obj - An object containing the image sprite URL, width and height.
+ * @property {string} sprite - The URL of the image sprite.
+ * @property {string} id - The id of the cute animal.
+ * @property {string} name - The name of the cute animal.
+ * @property {number} clicks - The number of clicks received by the cute animal.
+ */
+ var CuteAnimal = function (obj) {
+  'use strict';
+  this.sprite = obj.sprite;
+  this.id = obj.id;
+  this.name = obj.name;
+  this.clicks = 0;
+  this.render();
+  var self = this;
+  $('.img-animal#' + this.id).click(function() {
+    self.clicks++;
+    $('.click-counter#' + this.id).text(self.clicks);
+  });
+};
+
+/**
+ * Draw the cute animal on the screen.
+ */
+CuteAnimal.prototype.render = function () {
+  'use strict';
+  var $img = $('<img>').attr({
+    'class': 'img-animal',
+    'id': this.id,
+    'alt': this.name,
+    'src': this.sprite
+  });
+  var $name = $('<span>').text('Clicks for ' + this.name + ': ');
+  var $counter = $('<span>').text(0).attr({
+    'class': 'click-counter',
+    'id': this.id
+  });
+  var $div = $('<div>').attr('class', 'set-container').append($img, $name, $counter);
+  $main.append($div);
+};
+
+// --------------------------------------------------
+// Kitten class
+// --------------------------------------------------
+/**
+ * Array of objects that will be used to instantiate a cute kitten
+ * @typedef {object} kittens
+ * @type {Object[]}
+ * @property {string} sprite - The URL of the image sprite.
+ * @property {string} id - The id of the cute animal.
+ * @property {string} name - The name of the cute animal.
+ * @property {number} clicks - The number of clicks received by the cute animal.
+ * @example
+ * var kittens = [
+ *   {
+ *     sprite: 'img/princessleia.jpg',
+ *     id: 'kitten-princessleia',
+ *     name: 'Princess Leia',
+ *     imgSrc: 'http://ww3.hdnux.com/photos/31/24/44/6637850/3/920x920.jpg'
+ *   }, {
+ *     sprite: 'img/doctorwho.jpg',
+ *     id: 'kitten-doctorwho',
+ *     name: 'The Doctor',
+ *     imgSrc: 'http://i1.wp.com/imagizer.imageshack.us/v2/1024x768q90/845/0cw8f.jpg?resize=693%2C460'
+ *   }
+ * ];
+ */
 var kittens = [
   {
     sprite: 'img/princessleia.jpg',
@@ -31,7 +113,25 @@ var kittens = [
   }
 ];
 
-$('.img-kitten#kitten-doctorwho').click(function(e) {
-  clicks++;
-  $('.click-counter#kitten-doctorwho').text(clicks);
-});
+/**
+ * @constructor
+ * @extends CuteAnimal
+ */
+var Kitten = function (obj) {
+  'use strict';
+  CuteAnimal.call(this, obj);
+};
+Kitten.prototype = Object.create(CuteAnimal.prototype); // subclass prototype delegation
+Kitten.prototype.constructor = Kitten; // reset constructor from CuteAnimal to Kitten
+
+// --------------------------------------------------
+// Instantiate objects
+// --------------------------------------------------
+/**
+ * Array of instances of the Kitten class
+ * @type {Kitten[]}
+ */
+var cats = [], i;
+for (i = 0; i < TOTAL_KITTENS; i++) {
+  cats.push(new Kitten(kittens[i]));
+}
