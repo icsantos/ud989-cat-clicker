ca214fca2cd68793806703d5dac8df744b5a92df
icsantos <isantos1206@gmail.com>
2015-11-28 01:10:50 -0600

    feat: Convert from Constructor design pattern to MVC design pattern
    * Remove hard-coded form elements
    * Remove superclass and subclass constructors
    * Add model, views and controller variables and methods

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Udacity [JavaScript Design Patterns](https://www.udacity.com/course/javascript-d
 
 ### Visuals
 - The application should display
-  - a list of at least 5 cats, listed by name
+  - a list of cats by name
   - an area to display the selected cat
 - In the cat display area, the following should be displayed
   - the cat's name
--- js/app.js
+++ js/app.js
@@ -1,154 +1,146 @@
-/**
- * The number of kittens to display
- * @constant
- * @type {number}
- * @default
- */
-var TOTAL_KITTENS = 5;
-
-/**
- * The container for displaying the cute animal
- * @type {jQuery}
- */
-var $main = $('main');
-
-// --------------------------------------------------
-// CuteAnimal superclass
-// --------------------------------------------------
-/**
- * @constructor
- * @classdesc A superclass of the cute animals we will use in the app.
- * @param {kittens} obj - An object containing the image sprite URL, width and height.
- * @property {string} sprite - The URL of the image sprite.
- * @property {string} id - The id of the cute animal.
- * @property {string} name - The name of the cute animal.
- * @property {number} clicks - The number of clicks received by the cute animal.
- * @property {string} html - The html for displaying the cute animal.
- */
- var CuteAnimal = function (obj) {
+$(function () {
   'use strict';
-  this.sprite = obj.sprite;
-  this.id = obj.id;
-  this.name = obj.name;
-  this.clicks = 0;
-  this.setHtml();
-};
 
-/**
- * HTML tags to draw the cute animal on the screen.
- */
-CuteAnimal.prototype.setHtml = function () {
-  'use strict';
-  var $img = $('<img>').attr({
-    'class': 'img-animal',
-    'id': this.id,
-    'alt': this.name,
-    'src': this.sprite
-  });
-  var $counter = $('<span>').text(0).attr({
-    'class': 'click-counter',
-    'id': this.id
-  });
-  var $name = $('<div>').text('Clicks for ' + this.name + ': ').append($counter);
-  this.html = $('<div>').attr('class', 'set-container').append($name, $img);
-};
+  var model = {
+    /**
+     * The number of kittens to display
+     * @constant
+     * @type {number}
+     * @default
+     */
+    TOTAL_KITTENS: 5,
 
-/**
- * Add event handler for the cute animal.
- */
-CuteAnimal.prototype.addEventHandler = function () {
-  var self = this;
-  $('.img-animal#' + this.id).on('click', function() {
-    self.clicks++;
-    $('.click-counter#' + this.id).text(self.clicks);
-  });
-};
+    /**
+     * Array of cute kittens
+     * @type {Object[]}
+     * @property {string} sprite - The URL of the image sprite.
+     * @property {string} id - The id of the cute animal.
+     * @property {string} name - The name of the cute animal.
+     * @property {number} clicks - The number of clicks received by the cute animal.
+     * @example
+     * var kittens = [
+     *   {
+     *     sprite: 'img/princessleia.jpg',
+     *     id: 'kitten-princessleia',
+     *     name: 'Princess Leia',
+     *     clicks: 0,
+     *     imgSrc: 'http://ww3.hdnux.com/photos/31/24/44/6637850/3/920x920.jpg'
+     *   }, {
+     *     sprite: 'img/doctorwho.jpg',
+     *     id: 'kitten-doctorwho',
+     *     name: 'The Doctor',
+     *     imgSrc: 'http://i1.wp.com/imagizer.imageshack.us/v2/1024x768q90/845/0cw8f.jpg?resize=693%2C460'
+     *   }
+     * ];
+     */
+    kittens: [{
+        sprite: 'img/princessleia.jpg',
+        id: 'kitten-princessleia',
+        name: 'Princess Leia',
+        imgSrc: 'http://ww3.hdnux.com/photos/31/24/44/6637850/3/920x920.jpg'
+      }, {
+        sprite: 'img/doctorwho.jpg',
+        id: 'kitten-doctorwho',
+        name: 'The Doctor',
+        imgSrc: 'http://i1.wp.com/imagizer.imageshack.us/v2/1024x768q90/845/0cw8f.jpg?resize=693%2C460'
+      }, {
+        sprite: 'img/gandalf.jpg',
+        id: 'kitten-gandalf',
+        name: 'Gandalf',
+        imgSrc: 'http://i0.wp.com/imagizer.imageshack.us/v2/1024x768q90/837/fjr7.jpg?resize=344%2C519'
+      }, {
+        sprite: 'img/daenerys.jpg',
+        id: 'kitten-daenerys',
+        name: 'Daenerys',
+        imgSrc: 'http://ww4.hdnux.com/photos/31/24/44/6637839/3/1024x1024.jpg'
+      }, {
+        sprite: 'img/kermit.jpg',
+        id: 'kitten-kermit',
+        name: 'Kermit',
+        imgSrc: 'http://i728.photobucket.com/albums/ww287/popcornkids/KittenFrogHat.jpg'
+      }
+    ]
+  };
 
-// --------------------------------------------------
-// Kitten class
-// --------------------------------------------------
-/**
- * Array of objects that will be used to instantiate a cute kitten
- * @typedef {object} kittens
- * @type {Object[]}
- * @property {string} sprite - The URL of the image sprite.
- * @property {string} id - The id of the cute animal.
- * @property {string} name - The name of the cute animal.
- * @property {number} clicks - The number of clicks received by the cute animal.
- * @example
- * var kittens = [
- *   {
- *     sprite: 'img/princessleia.jpg',
- *     id: 'kitten-princessleia',
- *     name: 'Princess Leia',
- *     imgSrc: 'http://ww3.hdnux.com/photos/31/24/44/6637850/3/920x920.jpg'
- *   }, {
- *     sprite: 'img/doctorwho.jpg',
- *     id: 'kitten-doctorwho',
- *     name: 'The Doctor',
- *     imgSrc: 'http://i1.wp.com/imagizer.imageshack.us/v2/1024x768q90/845/0cw8f.jpg?resize=693%2C460'
- *   }
- * ];
- */
-var kittens = [
-  {
-    sprite: 'img/princessleia.jpg',
-    id: 'kitten-princessleia',
-    name: 'Princess Leia',
-    imgSrc: 'http://ww3.hdnux.com/photos/31/24/44/6637850/3/920x920.jpg'
-  }, {
-    sprite: 'img/doctorwho.jpg',
-    id: 'kitten-doctorwho',
-    name: 'The Doctor',
-    imgSrc: 'http://i1.wp.com/imagizer.imageshack.us/v2/1024x768q90/845/0cw8f.jpg?resize=693%2C460'
-  }, {
-    sprite: 'img/gandalf.jpg',
-    id: 'kitten-gandalf',
-    name: 'Gandalf',
-    imgSrc: 'http://i0.wp.com/imagizer.imageshack.us/v2/1024x768q90/837/fjr7.jpg?resize=344%2C519'
-  }, {
-    sprite: 'img/daenerys.jpg',
-    id: 'kitten-daenerys',
-    name: 'Daenerys',
-    imgSrc: 'http://ww4.hdnux.com/photos/31/24/44/6637839/3/1024x1024.jpg'
-  }, {
-    sprite: 'img/kermit.jpg',
-    id: 'kitten-kermit',
-    name: 'Kermit',
-    imgSrc: 'http://i728.photobucket.com/albums/ww287/popcornkids/KittenFrogHat.jpg'
-  }
-];
+  var controller = {
+    init: function () {
+      var i;
+      for (i = 0; i < model.TOTAL_KITTENS; i++) {
+        model.kittens[i].clicks = 0;
+        model.kittens[i].html = clickCounterView.init(model.kittens[i]);
+        pickListView.init(model.kittens[i]);
+      }
 
-/**
- * @constructor
- * @extends CuteAnimal
- */
-var Kitten = function (obj) {
-  'use strict';
-  CuteAnimal.call(this, obj);
-};
-Kitten.prototype = Object.create(CuteAnimal.prototype); // subclass prototype delegation
-Kitten.prototype.constructor = Kitten; // reset constructor from CuteAnimal to Kitten
+      pickListView.render();
+    },
 
-// --------------------------------------------------
-// Instantiate objects
-// --------------------------------------------------
-/**
- * Array of instances of the Kitten class
- * @type {Kitten[]}
- */
-var cats = [],
-  i;
-for (i = 0; i < TOTAL_KITTENS; i++) {
-  cats.push(new Kitten(kittens[i]));
-}
+    updateClicks: function (id) {
+      var i;
+      for (i = 0; i < model.TOTAL_KITTENS; i++) {
+        if (model.kittens[i].id === id) {
+          return ++model.kittens[i].clicks;
+        }
+      }
+      return 0;
+    },
 
-var showSelected = function() {
-  var id = $('input[name="animal"]:checked').val();
-  var obj = $.grep(cats, function (e) { return e.id === id; })[0];
-  $('.click-container').html(obj.html);
-  obj.addEventHandler();
-};
+    showSelected: function (id) {
+      var cuteAnimal = $.grep(model.kittens, function (e) {
+          return e.id === id;
+        })[0];
+      clickCounterView.render(cuteAnimal);
+    }
+  };
 
-$('#form-container input[type=radio]').on('change', showSelected);
+  var pickListView = {
+    showSelected: function () {
+      var id = $('input[name="animal"]:checked').val();
+      controller.showSelected(id);
+    },
 
+    init: function (cuteAnimal) {
+      var $input = $('<input>').attr({
+          'type': 'radio',
+          'name': 'animal',
+          'value': cuteAnimal.id,
+          'id': cuteAnimal.id
+        });
+      var $label = $('<label>').attr('for', cuteAnimal.id).text(cuteAnimal.name);
+      var $br = $('<br>');
+      $('#form-container').append($input, $label, $br);
+    },
+    
+    render: function () {
+      $('#form-container input[type=radio]').on('change', pickListView.showSelected);
+    }
+  };
+
+  var clickCounterView = {
+    init: function (cuteAnimal) {
+      var $img = $('<img>').attr({
+          'class': 'img-animal',
+          'id': cuteAnimal.id,
+          'alt': cuteAnimal.name,
+          'src': cuteAnimal.sprite
+        });
+      var $counter = $('<span>').text(0).attr({
+          'class': 'click-counter',
+          'id': cuteAnimal.id
+        });
+      var $name = $('<div>').text('Clicks for ' + cuteAnimal.name + ': ').append($counter);
+      var $html = $('<div>').attr('class', 'set-container').append($name, $img);
+      return $html;
+    },
+
+    render: function (cuteAnimal) {
+      $('.click-container').html(cuteAnimal.html);
+      $('.img-animal#' + cuteAnimal.id).on('click', function () {
+        var clicks = controller.updateClicks(cuteAnimal.id);
+        $('.click-counter#' + cuteAnimal.id).text(clicks);
+      });
+
+    }
+  };
+
+  controller.init();
+}());
