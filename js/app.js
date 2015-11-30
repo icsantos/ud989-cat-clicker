a270ad6350b8df52f5409d7d5d99ee557727b1e8
icsantos <isantos1206@gmail.com>
2015-11-30 06:15:39 -0600

    feat: Add functionality for the admin form

--- js/app.js
+++ js/app.js
@@ -1,38 +1,9 @@
-$(function () {
+$(function() {
   'use strict';
 
   var model = {
-    /**
-     * The number of kittens to display
-     * @constant
-     * @type {number}
-     * @default
-     */
     TOTAL_KITTENS: 5,
 
-    /**
-     * Array of cute kittens
-     * @type {Object[]}
-     * @property {string} sprite - The URL of the image sprite.
-     * @property {string} id - The id of the cute animal.
-     * @property {string} name - The name of the cute animal.
-     * @property {number} clicks - The number of clicks received by the cute animal.
-     * @example
-     * var kittens = [
-     *   {
-     *     sprite: 'img/princessleia.jpg',
-     *     id: 'kitten-princessleia',
-     *     name: 'Princess Leia',
-     *     clicks: 0,
-     *     imgSrc: 'http://ww3.hdnux.com/photos/31/24/44/6637850/3/920x920.jpg'
-     *   }, {
-     *     sprite: 'img/doctorwho.jpg',
-     *     id: 'kitten-doctorwho',
-     *     name: 'The Doctor',
-     *     imgSrc: 'http://i1.wp.com/imagizer.imageshack.us/v2/1024x768q90/845/0cw8f.jpg?resize=693%2C460'
-     *   }
-     * ];
-     */
     kittens: [{
         sprite: 'img/princessleia.jpg',
         id: 'kitten-princessleia',
@@ -59,11 +30,26 @@ $(function () {
         name: 'Kermit',
         imgSrc: 'http://i728.photobucket.com/albums/ww287/popcornkids/KittenFrogHat.jpg'
       }
-    ]
+    ],
+
+    selected: -1,
+
+    setSelected: function(id) {
+      var i;
+      for (i = 0; i < model.TOTAL_KITTENS; i++) {
+        if (model.kittens[i].id === id) {
+          model.selected = i;
+        }
+      }
+    },
+
+    updateClicks: function() {
+      model.kittens[model.selected].clicks++;
+    }
   };
 
   var controller = {
-    init: function () {
+    init: function() {
       var i;
       for (i = 0; i < model.TOTAL_KITTENS; i++) {
         model.kittens[i].clicks = 0;
@@ -72,33 +58,48 @@ $(function () {
       }
 
       pickListView.render();
+      adminView.init();
     },
 
-    updateClicks: function (id) {
-      var i;
-      for (i = 0; i < model.TOTAL_KITTENS; i++) {
-        if (model.kittens[i].id === id) {
-          return ++model.kittens[i].clicks;
-        }
-      }
-      return 0;
-    },
-
-    showSelected: function (id) {
-      var cuteAnimal = $.grep(model.kittens, function (e) {
-          return e.id === id;
-        })[0];
+    showSelected: function(id) {
+      model.setSelected(id);
+      var cuteAnimal = model.kittens[model.selected];
       clickCounterView.render(cuteAnimal);
+      if (adminView.buttonState === 'hide') {
+        adminView.buttonState = 'show';
+        adminView.render();
+      }
+      adminView.setValues(cuteAnimal);
+    },
+
+    updateSelected: function() {
+      model.kittens[model.selected].name = adminView.fields.name.val();
+      model.kittens[model.selected].imgurl = adminView.fields.imgurl.val();
+      model.kittens[model.selected].clicks = adminView.fields.clicks.val();
+
+      var cuteAnimal = model.kittens[model.selected];
+      model.kittens[model.selected].html = clickCounterView.init(cuteAnimal);
+      clickCounterView.render(cuteAnimal);
+    },
+
+    resetFields: function() {
+      adminView.setValues(model.kittens[model.selected]);
+    },
+
+    updateClicks: function() {
+      model.updateClicks();
+      adminView.setValues(model.kittens[model.selected]);
+      return model.kittens[model.selected].clicks;
     }
   };
 
   var pickListView = {
-    showSelected: function () {
+    showSelected: function() {
       var id = $('input[name="animal"]:checked').val();
       controller.showSelected(id);
     },
 
-    init: function (cuteAnimal) {
+    init: function(cuteAnimal) {
       var $input = $('<input>').attr({
           'type': 'radio',
           'name': 'animal',
@@ -109,21 +110,58 @@ $(function () {
       var $br = $('<br>');
       $('#picklist-container').append($input, $label, $br);
     },
-    
-    render: function () {
+
+    render: function() {
       $('#picklist-container input[type=radio]').on('change', pickListView.showSelected);
     }
   };
 
+  var adminView = {
+    buttonState: 'hide',
+    button: $('#admin-button'),
+    fieldset: $('.admin-fieldset'),
+    fields: {
+      name: $('.admin-fieldset #kitten-name'),
+      imgurl: $('.admin-fieldset #kitten-imgurl'),
+      clicks: $('.admin-fieldset #kitten-clicks')
+    },
+
+    init: function() {
+      adminView.button.hide();
+      adminView.fieldset.hide();
+    },
+
+    render: function() {
+      $(adminView.button).show();
+      $(adminView.button).on('click', function() {
+        $(adminView.fieldset).toggle('fast');
+      });
+      $('#admin-container').on('submit', function(event) {
+        controller.updateSelected();
+        event.preventDefault();
+      });
+      $('#admin-container').on('reset', function(event) {
+        controller.resetFields();
+        event.preventDefault();
+      });
+    },
+
+    setValues: function(cuteAnimal) {
+      $(adminView.fields.name).val(cuteAnimal.name);
+      $(adminView.fields.imgurl).val(cuteAnimal.sprite);
+      $(adminView.fields.clicks).val(cuteAnimal.clicks);
+    },
+  };
+
   var clickCounterView = {
-    init: function (cuteAnimal) {
+    init: function(cuteAnimal) {
       var $img = $('<img>').attr({
           'class': 'img-animal',
           'id': cuteAnimal.id,
           'alt': cuteAnimal.name,
           'src': cuteAnimal.sprite
         });
-      var $counter = $('<span>').text(0).attr({
+      var $counter = $('<span>').text(cuteAnimal.clicks).attr({
           'class': 'click-counter',
           'id': cuteAnimal.id
         });
@@ -132,13 +170,12 @@ $(function () {
       return $html;
     },
 
-    render: function (cuteAnimal) {
+    render: function(cuteAnimal) {
       $('.click-container').html(cuteAnimal.html);
-      $('.img-animal#' + cuteAnimal.id).on('click', function () {
-        var clicks = controller.updateClicks(cuteAnimal.id);
+      $('.img-animal#' + cuteAnimal.id).on('click', function() {
+        var clicks = controller.updateClicks();
         $('.click-counter#' + cuteAnimal.id).text(clicks);
       });
-
     }
   };
 
